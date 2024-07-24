2c5d081a90156c0971786a45bcb68269dc492e4c
getkeshav <20bcs111@nith.ac.in>
2024-07-25 01:58:00 +0530

    Updated code

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -24,7 +24,7 @@ app.use(express.static('public'));
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname,'public', 'index.html'))
 })
-
+//
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log("Website served on http://localhost:" + port);
